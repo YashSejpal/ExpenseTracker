d2a1d3a464c2260e669fab85992c63005346281c
ysejpal <yhsejpal@gmail.com>
2025-07-02 16:28:17 +0530

    Adding comments

--- src/AuthService.java
+++ src/AuthService.java
@@ -4,7 +4,7 @@ import org.json.*;
 
 public class AuthService {
     private static final String USERS_FILE = "users.json";
-
+    //This function takes username as input, and checks the users.json file for any existing user. Then it creates a 6 digit login code.
     public boolean register(String username) throws IOException, JSONException {
         JSONObject users = loadUsers();
         if (users.has(username)) return false;
@@ -16,12 +16,12 @@ public class AuthService {
         System.out.println("Your login code (save this!): " + loginCode);
         return true;
     }
-
+    //This function takes the username and login code as input and authenticates using the users.json file
     public boolean login(String username, String loginCode) throws IOException, JSONException {
         JSONObject users = loadUsers();
         return users.has(username) && users.getString(username).equals(loginCode);
     }
-
+    //This function reads the users.json file and returns a JSON object containing all the entries
     private JSONObject loadUsers() throws IOException, JSONException {
         File file = new File(USERS_FILE);
         if (!file.exists()) return new JSONObject();
@@ -33,7 +33,7 @@ public class AuthService {
         reader.close();
         return new JSONObject(sb.toString());
     }
-
+    //This function writes the users back to users.json file
     private void saveUsers(JSONObject users) throws IOException {
         BufferedWriter writer = new BufferedWriter(new FileWriter(USERS_FILE));
         writer.write(users.toString(4));
